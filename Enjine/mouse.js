e6bef0c92882f61577180e63771e048f68ec4133
Rob Kleffner <gamecodeshark@gmail.com>
2011-06-06 21:56:34 -0600

    First version of mouse input.

---
+++ Enjine/mouse.js
@@ -0,0 +1,43 @@
+/**
+	Class that helps to manage mouse input.
+	Code by Rob Kleffner, 2011
+*/
+
+$e.Buttons = {
+    MouseLeft: 0,
+	MouseRight: 2,
+	MouseWheel: 1,
+};
+
+$e.Mouse = {
+	x: 0,
+	y: 0,
+    pressed: [],
+    
+    initialize: function(element) {
+        var self = this;
+        element.onmousedown = function(event) { self.mouseDownEvent(event); };
+        element.onmouseup = function(event) { self.mouseUpEvent(event); };
+		element.onmousemove = function(event) { self.mouseMoveEvent(event); };
+    },
+    
+    isButtonDown: function(key) {
+        if (this.pressed[key] !== null) {
+            return this.pressed[key];
+		}
+        return false;
+    },
+    
+    mouseDownEvent: function(event) {
+        this.pressed[event.button] = true;
+    },
+    
+    mouseUpEvent: function(event) {
+        this.pressed[event.button] = false;
+    },
+	
+	mouseMoveEvent: function(event) {
+		this.x = event.clientX;
+		this.y = event.clientY;
+	}
+};
