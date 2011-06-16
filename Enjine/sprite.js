94a715c7f5fc5dc9168eda877090ec5ac606dda0
Rob Kleffner <rob@metageek.net>
2011-06-16 10:48:46 -0600

    Sprites are now positioned relative to their center, and support scaling and rotation. Beginning the same for animated sprites.

--- Enjine/sprite.js
+++ Enjine/sprite.js
@@ -6,11 +6,22 @@
 $e.Sprite = function() {
 	this.x = 0;
 	this.y = 0;
+	this.xPivot = 0.5;
+	this.yPivot = 0.5;
+	this.angle = 0;
+	this.xScale = 1;
+	this.yScale = 1;
 	this.image = null;
 };
 
 $e.Sprite.prototype = new $e.Drawable();
 
 $e.Sprite.prototype.draw = function(context, camera) {
-	context.drawImage(this.image, this.x - camera.x, this.y - camera.y);
+	this.angle += 0.1;
+	context.save();
+	context.translate(this.x, this.y);
+	context.scale(this.xScale, this.yScale);
+	context.rotate(this.angle);
+	context.drawImage(this.image, -this.image.width * this.xPivot, -this.image.height * this.yPivot);
+	context.restore();
 };
